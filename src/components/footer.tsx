3bbf28e62341fe0dc789b4814ec9f8c6043918fd
Mert Guler <gulermert94@gmail.com>
2023-03-30 04:46:47 -0400

    fix footer on mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,19 +14,17 @@ export const Footer = () => {
     setIsOpen(true);
   }
 
+  if (!isSignedIn) return null;
+
   return (
     <div className="flex items-center justify-center border border-slate-100 p-4">
-      {isSignedIn && (
-        <>
-          <button
-            className="h-6 w-6 rounded-lg bg-slate-300 text-slate-800 hover:bg-slate-400"
-            onClick={openModal}
-          >
-            +
-          </button>
-          <UploadPictureModal isOpen={isOpen} closeModal={closeModal} />
-        </>
-      )}
+      <button
+        className="h-6 w-6 rounded-lg bg-slate-300 text-slate-800 hover:bg-slate-400"
+        onClick={openModal}
+      >
+        +
+      </button>
+      <UploadPictureModal isOpen={isOpen} closeModal={closeModal} />
     </div>
   );
 };
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,10 +2,12 @@ import type { PropsWithChildren } from "react";
 
 export const Layout = (props: PropsWithChildren) => {
   return (
-    <main className="flex h-screen justify-center overflow-hidden">
-      <div className="flex h-full w-full flex-col justify-between border-x border-slate-400 md:max-w-2xl">
-        {props.children}
-      </div>
-    </main>
+    <div className="absolute inset-0">
+      <main className="flex h-screen justify-center overflow-hidden">
+        <div className="flex h-full w-full flex-col justify-between border-x border-slate-400 md:max-w-2xl">
+          {props.children}
+        </div>
+      </main>
+    </div>
   );
 };
